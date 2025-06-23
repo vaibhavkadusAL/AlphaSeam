508ccf421e0093d6557b589a1efb27ecd68370b6
Tejas Mahakal <tejasmahakal@gtasterix.com>
2025-06-23 15:36:29 +0530

    updated

--- alphaseam/src/App.jsx
+++ alphaseam/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div>
-      <header />
+      <Header/>
+      <Footer/>
     </div>
   )
 }
